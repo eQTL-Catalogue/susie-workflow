186a1b459d556c3c4b4bb6a56884fca6c9be6946
Kaur Alasoo <kaur.alasoo@gmail.com>
2020-05-17 15:09:13 +0300

    Remove old examples

--- example.sh
+++
@@ -1,9 +0,0 @@
-
-nextflow run main.nf --qtl_results /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/finemapping/Alasoo_2018.tsv\
- --cisdistance 1000000 \
- --n_batches 200\
- --eqtlutils '/gpfs/hpc/home/a72094/projects/eQTLUtils'\
- -resume\
- -profile finemapping\
- -executor.queueSize 1
-
