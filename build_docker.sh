de579f0c2a2a57883a9f0a0357bfd2cf6f5e1ec5
Kaur Alasoo <kaur.alasoo@gmail.com>
2020-09-08 01:35:58 +0300

    Update build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -1,8 +1,8 @@
 ### Build the Docker container
-sudo docker build -t kauralasoo/susie-finemapping .
+sudo docker build -t eqtlcatalogue/susie-finemapping:v20.08.1 .
 
 ### Push to DockerHub
-docker push kauralasoo/susie-finemapping
+docker push eqtlcatalogue/susie-finemapping:v20.08.1
 
 ### Build a local copy of the Singularity container
 singularity build susie-finemapping.img docker://kauralasoo/susie-finemapping:latest
